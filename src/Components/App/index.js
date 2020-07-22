0f0983aaea296217a413c528a951f4ee5d00a793
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-07-22 21:15:05 +0100

    Install prettier eslint an add eslintrc json file

---
+++ src/Components/App/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+
+const App = () => {
+    return <div className='App'>
+            <div className="top">4:43</div>
+            <div className="display">0</div>
+            <div className="buttons">AC</div>
+            <div className="bottom">-</div> 
+        </div>;
+}
+
+export default App;
