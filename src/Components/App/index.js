6dd02dfd212c5387368ff6881a1a5a3bcd8b90d5
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-07-23 09:05:13 +0100

    Update

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "../Button/Button";
 import './App.css';
 
 const App = () => {
@@ -6,6 +7,10 @@ const App = () => {
             <div className="top">4:43</div>
             <div className="display">0</div>
             <div className="buttons">AC</div>
+                <Button  content="AC"/>
+                <Button  content="±"/>
+                <Button  content="%"/>
+                <Button  content="÷"/>
             <div className="bottom">-</div> 
         </div>;
 }
