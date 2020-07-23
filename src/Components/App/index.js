1f03fe30fc29f7e0a680597580e6354f70da1730
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-07-23 10:32:17 +0100

    More symbols and number

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -6,11 +6,27 @@ const App = () => {
     return <div className='App'>
             <div className="top">4:43</div>
             <div className="display">0</div>
-            <div className="buttons">AC</div>
+            <div className="buttons">
                 <Button  content="AC"/>
                 <Button  content="±"/>
                 <Button  content="%"/>
                 <Button  content="÷"/>
+                <Button  content="7"/>
+                <Button  content="8"/>
+                <Button  content="9"/>
+                <Button  content="×"/>
+                <Button  content="4"/>
+                <Button  content="5"/>
+                <Button  content="6"/>
+                <Button  content="−"/>
+                <Button  content="1"/>
+                <Button  content="2"/>
+                <Button  content="3"/>
+                <Button  content="+"/>
+                <Button  content="0"/>
+                <Button  content="."/>
+                <Button  content="="/>
+            </div>
             <div className="bottom">-</div> 
         </div>;
 }
