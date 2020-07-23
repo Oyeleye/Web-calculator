4ef5cc32759bf1ea6e82786d8628fe20accceb33
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-07-23 13:05:20 +0100

    parseFloat and conditional statement

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -5,7 +5,19 @@ import './App.css';
 const App = () => {
 const [value, setValue] = React.useState("0");
 
-const handleButtonPress = () =>{};
+const handleButtonPress = content => () =>{
+    const num = parseFloat(value);
+
+    if (content === 'AC') {
+        setValue('0')
+        return;
+    }
+    if (content === '±') {
+        setValue( (num * -1).toString());
+        return;
+    }
+    setValue( parseFloat(num + content).toString());
+};
 
     return <div className='App'>
             <div className="top">4:43</div>
