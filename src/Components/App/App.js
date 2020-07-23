334d50ecfdd6d39fa2e29523ae8da50dd363b8f9
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-07-23 19:06:54 +0100

    Change 2nd index js to App js

---
+++ src/Components/App/App.js
@@ -0,0 +1,112 @@
+import React, {useState} from "react";
+import Button from "../Button/Button";
+import './App.css';
+
+const App = () => {
+    const [value, setValue] = useState("0");
+    const [memory, setMemory] = useState(null);
+    const [operator, setOperator] = useState(null);
+
+    const handleButtonPress = content => () =>{
+        const num = parseFloat(value);
+
+    if (content === "AC") {
+        setValue("0");
+        setMemory(null);
+        setOperator(null);
+        return;
+    }
+    if (content === "±") {
+        setValue((num * -1).toString());
+        return;
+    }
+    if (content === "%") {
+        setValue((num / "100" ).toString());
+        setMemory(null);
+        return;
+    }
+    if (content === "+") {
+        setMemory(parseFloat(value));
+        setValue("0");
+        setOperator("+");
+        return;
+    }
+    if (content === "−") {
+        setMemory(parseFloat(value));
+        setValue("0");
+        setOperator("−");
+        return;
+    }
+    if (content === "×") {
+        setMemory(parseFloat(value));
+        setValue("0");
+        setOperator("×");
+        return;
+    }
+    if (content === "%") {
+        setMemory(parseFloat(value));
+        setValue("0");
+        setOperator("%");
+        return;
+    }
+    if (content === "÷") {
+        setMemory(parseFloat(value));
+        setValue("0");
+        setOperator("÷");
+        return;
+    }
+    if (content === "=") {
+        if (!operator) return;
+        else if (operator === "+") {
+            setValue((memory + parseFloat(value)).toString());
+        }
+        else if (operator === "−") {
+            setValue((memory - parseFloat(value)).toString());
+        }
+        else if (operator === "×") {
+            setValue((memory * parseFloat(value)).toString());
+        }
+        else if (operator === "÷") {
+            setValue((memory / parseFloat(value)).toString());
+        }
+        setMemory(null);
+        return;
+    }
+    setValue(parseFloat(num + content).toString());
+};
+
+    return(
+            <div className='App'>
+            <div className="top">4:43</div>
+            <div className="display">{value}</div>
+            <div className="buttons">
+                <Button 
+                    onButtonClick={handleButtonPress} 
+                    content="AC" 
+                    type="function" 
+                />
+                <Button onButtonClick={handleButtonPress} content="±" type="function" />
+                <Button onButtonClick={handleButtonPress} content="%" type="function" />
+                <Button onButtonClick={handleButtonPress} content="÷" type="operator" />
+                <Button onButtonClick={handleButtonPress} content="7" />
+                <Button onButtonClick={handleButtonPress} content="8" />
+                <Button onButtonClick={handleButtonPress} content="9" />
+                <Button onButtonClick={handleButtonPress} content="×" type="operator" />
+                <Button onButtonClick={handleButtonPress} content="4" />
+                <Button onButtonClick={handleButtonPress} content="5" />
+                <Button onButtonClick={handleButtonPress} content="6" />
+                <Button onButtonClick={handleButtonPress} content="−" type="operator" />
+                <Button onButtonClick={handleButtonPress} content="1" />
+                <Button onButtonClick={handleButtonPress} content="2" />
+                <Button onButtonClick={handleButtonPress} content="3" />
+                <Button onButtonClick={handleButtonPress} content="+" type="operator" />
+                <Button onButtonClick={handleButtonPress} content="0" />
+                <Button onButtonClick={handleButtonPress} content="." />
+                <Button onButtonClick={handleButtonPress} content="=" type="operator" />
+            </div>
+            <div className="bottom">-</div> 
+        </div>
+    );
+}
+
+export default App;
