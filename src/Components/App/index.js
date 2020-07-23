d9e7202494a8ce573a3556c11c89f43e1bef1b2b
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-07-23 11:33:23 +0100

    Add Button type

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -7,25 +7,25 @@ const App = () => {
             <div className="top">4:43</div>
             <div className="display">0</div>
             <div className="buttons">
-                <Button  content="AC"/>
-                <Button  content="±"/>
-                <Button  content="%"/>
-                <Button  content="÷"/>
-                <Button  content="7"/>
-                <Button  content="8"/>
-                <Button  content="9"/>
-                <Button  content="×"/>
-                <Button  content="4"/>
-                <Button  content="5"/>
-                <Button  content="6"/>
-                <Button  content="−"/>
-                <Button  content="1"/>
-                <Button  content="2"/>
-                <Button  content="3"/>
-                <Button  content="+"/>
-                <Button  content="0"/>
-                <Button  content="."/>
-                <Button  content="="/>
+                <Button  content="AC" type="function" />
+                <Button  content="±" type="function" />
+                <Button  content="%" type="function" />
+                <Button  content="÷" type="operator" />
+                <Button  content="7" />
+                <Button  content="8" />
+                <Button  content="9" />
+                <Button  content="×" type="operator" />
+                <Button  content="4" />
+                <Button  content="5" />
+                <Button  content="6" />
+                <Button  content="−" type="operator" />
+                <Button  content="1" />
+                <Button  content="2" />
+                <Button  content="3" />
+                <Button  content="+" type="operator" />
+                <Button  content="0" />
+                <Button  content="." />
+                <Button  content="=" type="operator" />
             </div>
             <div className="bottom">-</div> 
         </div>;
