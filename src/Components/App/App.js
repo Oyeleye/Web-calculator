195fb20072c24237cd1b5c54f87dd507e63e4ef2
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-07-25 16:38:48 +0100

    Add body div

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -135,35 +135,37 @@ const App = () => {
 };
 
     return(
-            <div className='App'>
-            <div className="top">4:43</div>
-            <div className="display">{value}</div>
-            <div className="buttons">
-                <Button 
-                    onButtonClick={handleButtonPress} 
-                    content="AC" 
-                    type="function" 
-                />
-                <Button onButtonClick={handleButtonPress} content="±" type="function" />
-                <Button onButtonClick={handleButtonPress} content="%" type="function" />
-                <Button onButtonClick={handleButtonPress} content="÷" type="operator" />
-                <Button onButtonClick={handleButtonPress} content="7" />
-                <Button onButtonClick={handleButtonPress} content="8" />
-                <Button onButtonClick={handleButtonPress} content="9" />
-                <Button onButtonClick={handleButtonPress} content="×" type="operator" />
-                <Button onButtonClick={handleButtonPress} content="4" />
-                <Button onButtonClick={handleButtonPress} content="5" />
-                <Button onButtonClick={handleButtonPress} content="6" />
-                <Button onButtonClick={handleButtonPress} content="−" type="operator" />
-                <Button onButtonClick={handleButtonPress} content="1" />
-                <Button onButtonClick={handleButtonPress} content="2" />
-                <Button onButtonClick={handleButtonPress} content="3" />
-                <Button onButtonClick={handleButtonPress} content="+" type="operator" />
-                <Button onButtonClick={handleButtonPress} content="0" />
-                <Button onButtonClick={handleButtonPress} content="." />
-                <Button onButtonClick={handleButtonPress} content="=" type="operator" />
+        <div className="Body">
+            <div className="App">
+                <div className="top">4:43</div>
+                <div className="display">{value}</div>
+                <div className="buttons">
+                    <Button 
+                        onButtonClick={handleButtonPress} 
+                        content="AC" 
+                        type="function" 
+                    />
+                    <Button onButtonClick={handleButtonPress} content="±" type="function" />
+                    <Button onButtonClick={handleButtonPress} content="%" type="function" />
+                    <Button onButtonClick={handleButtonPress} content="÷" type="operator" />
+                    <Button onButtonClick={handleButtonPress} content="7" />
+                    <Button onButtonClick={handleButtonPress} content="8" />
+                    <Button onButtonClick={handleButtonPress} content="9" />
+                    <Button onButtonClick={handleButtonPress} content="×" type="operator" />
+                    <Button onButtonClick={handleButtonPress} content="4" />
+                    <Button onButtonClick={handleButtonPress} content="5" />
+                    <Button onButtonClick={handleButtonPress} content="6" />
+                    <Button onButtonClick={handleButtonPress} content="−" type="operator" />
+                    <Button onButtonClick={handleButtonPress} content="1" />
+                    <Button onButtonClick={handleButtonPress} content="2" />
+                    <Button onButtonClick={handleButtonPress} content="3" />
+                    <Button onButtonClick={handleButtonPress} content="+" type="operator" />
+                    <Button onButtonClick={handleButtonPress} content="0" />
+                    <Button onButtonClick={handleButtonPress} content="." />
+                    <Button onButtonClick={handleButtonPress} content="=" type="operator" />
+                </div>
+                <div className="bottom">-</div> 
             </div>
-            <div className="bottom">-</div> 
         </div>
     );
 }
