495d85544f878be7787a097803e09816056954af
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-07-26 14:06:03 +0100

    Add time

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,6 +6,7 @@ const App = () => {
     const [value, setValue] = useState("0");
     const [memory, setMemory] = useState(null);
     const [operator, setOperator] = useState(null);
+    const [time, setTime] = useState(new Date());
 
     const handleButtonPress = content => () =>{
         const num = parseFloat(value);
@@ -23,6 +24,11 @@ const App = () => {
     if (content === "%") {
         setValue((num / "100" ).toString());
         setMemory(null);
+        return;
+    }
+    if (content === ".") {
+        if (value.includes(2.2)) return;
+        setValue(value + ".");
         return;
     }
     if (content === "+") {
@@ -72,7 +78,8 @@ const App = () => {
             }   else if (operator === "÷") {
                 setMemory((memory / parseFloat(value)));
             }
-        }   else  {
+        }
+           else  {
             setMemory(parseFloat(value));
         }
         setValue("0");
@@ -129,44 +136,57 @@ const App = () => {
         }
 
         setMemory(null);
+        setOperator(null);
         return;
     }
+    if(value[value.length-1] ===".") {
+        setValue(value + content)
+    }else {
     setValue(parseFloat(num + content).toString());
-};
+    }
+};  
+
 
     return(
-        <div className="Body">
-            <div className="App">
-                <div className="top">4:43</div>
-                <div className="display">{value}</div>
-                <div className="buttons">
-                    <Button 
-                        onButtonClick={handleButtonPress} 
-                        content="AC" 
-                        type="function" 
-                    />
-                    <Button onButtonClick={handleButtonPress} content="±" type="function" />
-                    <Button onButtonClick={handleButtonPress} content="%" type="function" />
-                    <Button onButtonClick={handleButtonPress} content="÷" type="operator" />
-                    <Button onButtonClick={handleButtonPress} content="7" />
-                    <Button onButtonClick={handleButtonPress} content="8" />
-                    <Button onButtonClick={handleButtonPress} content="9" />
-                    <Button onButtonClick={handleButtonPress} content="×" type="operator" />
-                    <Button onButtonClick={handleButtonPress} content="4" />
-                    <Button onButtonClick={handleButtonPress} content="5" />
-                    <Button onButtonClick={handleButtonPress} content="6" />
-                    <Button onButtonClick={handleButtonPress} content="−" type="operator" />
-                    <Button onButtonClick={handleButtonPress} content="1" />
-                    <Button onButtonClick={handleButtonPress} content="2" />
-                    <Button onButtonClick={handleButtonPress} content="3" />
-                    <Button onButtonClick={handleButtonPress} content="+" type="operator" />
-                    <Button onButtonClick={handleButtonPress} content="0" />
-                    <Button onButtonClick={handleButtonPress} content="." />
-                    <Button onButtonClick={handleButtonPress} content="=" type="operator" />
+            <>
+            <div className="Body">
+                <div className="App">
+                    <div className="top">
+                        <div className="time">
+                            {time.getHours()}:{time.getMinutes()}
+                        </div>
+                    </div>
+                    <div className="display">{value}</div>
+                    <div className="buttons">
+                        <Button 
+                            onButtonClick={handleButtonPress} 
+                            content="AC" 
+                            type="function" 
+                        />
+                        <Button onButtonClick={handleButtonPress} content="±" type="function" />
+                        <Button onButtonClick={handleButtonPress} content="%" type="function" />
+                        <Button onButtonClick={handleButtonPress} content="÷" type="operator" />
+                        <Button onButtonClick={handleButtonPress} content="7" />
+                        <Button onButtonClick={handleButtonPress} content="8" />
+                        <Button onButtonClick={handleButtonPress} content="9" />
+                        <Button onButtonClick={handleButtonPress} content="×" type="operator" />
+                        <Button onButtonClick={handleButtonPress} content="4" />
+                        <Button onButtonClick={handleButtonPress} content="5" />
+                        <Button onButtonClick={handleButtonPress} content="6" />
+                        <Button onButtonClick={handleButtonPress} content="−" type="operator" />
+                        <Button onButtonClick={handleButtonPress} content="1" />
+                        <Button onButtonClick={handleButtonPress} content="2" />
+                        <Button onButtonClick={handleButtonPress} content="3" />
+                        <Button onButtonClick={handleButtonPress} content="+" type="operator" />
+                        <Button onButtonClick={handleButtonPress} content="0" />
+                        <Button onButtonClick={handleButtonPress} content="." />
+                        <Button onButtonClick={handleButtonPress} content="=" type="operator" />
+                    </div>
+                    <div className="bottom">-</div> 
                 </div>
-                <div className="bottom">-</div> 
             </div>
-        </div>
+
+            </>
     );
 }
 
